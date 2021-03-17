c627b4d0ca9f8d954e65d72610f781b9b1883d70
Kwanpon Wattanaklang <kwanpon.w@touchtechnologies.co.th>
2021-03-17 14:15:49 +0700

    chore

--- kafka/broker.go
+++ kafka/broker.go
@@ -60,9 +60,8 @@ func (kafka *broker) SendTopicMessage(topic string, msg []byte) (err error) {
 
 func (kafka *broker) Start(closeCallback common.CloseCallback, ch chan bool) {
 	go kafka.startConsumingLoop()
-	kafka.waitUntilConsumerIsReady()
+	kafka.waitUntilConsumerIsReady(ch)
 	kafka.waitForCloseSignal(closeCallback)
-	ch <- true
 }
 
 func (kafka *broker) startConsumingLoop() {
@@ -90,8 +89,9 @@ func (kafka *broker) contextIsCancelled() (is bool) {
 	return kafka.ctx.Err() != nil
 }
 
-func (kafka *broker) waitUntilConsumerIsReady() {
+func (kafka *broker) waitUntilConsumerIsReady(ch chan bool) {
 	<-kafka.consumer.ready
+	ch <- true
 }
 func (kafka *broker) waitUntilCleanupIsReady() (err error) {
 	err = <-kafka.cleanupReady
